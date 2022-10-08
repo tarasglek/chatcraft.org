b86994a32aa8f51853b0075d258873e81fb68b8c
Taras Glek <taras@glek.net>
2022-10-08 14:48:21 +0300

    basic filename stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,59 @@ import Editor from "react-simple-code-editor";
 import {highlight, languages} from 'prismjs'
 import "prismjs/themes/prism.css";
 import "prismjs/components/prism-markdown"
+import { get, set } from 'idb-keyval';
+
 // import logo from './logo.svg';
 import './App.css';
 
-const code = `function add(a, b) {
-  return a + b;
-}
+const code = `
+Javascript code to find all hyperlinks in a page is
+\`\`\`javascript
 
-const a = 123;
 `;
 
 console.log(languages)
 
 function App() {
+  const [filename, _setFilename] = useState('')
   const [codeValue, setCodeValue] = useState(code);
+
+  window.addEventListener("hashchange", function(e){
+    _setFilename(window.location.hash.substring(1))
+  });
+  
+  function setFilename(filename: string) {
+    filename.replaceAll('#', '')
+    window.location.hash = filename
+    _setFilename(filename)
+  }
+
+  function promptSaveAs() {
+    let newHash = prompt('How would you like to name you workspace', 'workspace')
+    if (newHash)
+      setFilename(newHash)
+  }
+
+  if (filename === '') {
+    setFilename('Untitled')
+  }
+
   return (
     <div> 
-    <Editor
-    value={codeValue}
-    onValueChange={code => setCodeValue(code)}
-    highlight={code => {
-      let x = highlight(code, languages.markdown, 'markdown');
-      return x;
-      }
-   }
-    padding={10}
-    textareaId="codeArea"
-    className="editor"
-    style={{
-      fontFamily: '"Fira code", "Fira Mono", monospace',
-      fontSize: 18,
-      outline: 0
-    }}
-  />
+      <div><strong style={{paddingRight:'1em'}}>{filename}</strong><button onClick={promptSaveAs}>Save As</button></div>
+      <Editor autoFocus
+      value={codeValue}
+      onValueChange={code => setCodeValue(code)}
+      highlight={code => highlight(code, languages.markdown, 'markdown')}
+      padding={10}
+      textareaId="codeArea"
+      className="editor"
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 18,
+        outline: 0
+      }}
+    />
   </div>
   );
 }
