74d499c851b253f3584eb26e2871366e86002aa7
Taras Glek <taras@glek.net>
2022-10-09 13:57:01 +0300

    cleaned up state

--- src/Code.tsx
+++ src/Code.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Editor from "react-simple-code-editor";
 import OpenAI from "openai-api";
 import {highlight, languages} from 'prismjs'
 import "prismjs/themes/prism.css";
 import "prismjs/components/prism-markdown"
 import {
-  BrowserRouter as Router,
-  useParams
+  useParams,
+  useNavigate
 } from "react-router-dom";
 import { get, set } from 'idb-keyval';
 // import logo from './logo.svg';
@@ -20,63 +20,68 @@ Javascript code to find all hyperlinks in a page is
 
 const SETTING_OPENAIKEY = '#openai-api-key'
 const SETTING_SAVED_FILES = '#saved_files'
-
+interface State {
+  code: string;
+  openaiToken: string;
+  loaded: boolean;
+  savedFiles: Set<string>;
+}
 function Code() {
-  let { slug } = useParams();
-  console.log(slug)
-  const [filename, _setFilename] = useState('')  
-  const [codeValue, _setCodeValue] = useState(defaultCode);
-  const [openaiKey, setOpenaiKey] = useState('')
-  const [loadedSettings, setLoadedSettings] = useState(false)
-  const [savedFiles, setSavedFiles] = useState(new Set<string>())
+  const [settings, setSettings] = useState<State>(
+    {
+      code: defaultCode,
+      openaiToken: '',
+      loaded: false,
+      savedFiles: new Set<string>(),
+    });
+  const _navigate = useNavigate();
+  let filename = useParams().id!;
+  async function flushSavedFiles () {
+    return set (SETTING_SAVED_FILES, settings.savedFiles)
+  }
   // per https://devtrium.com/posts/async-functions-useeffect
   useEffect(() => {
     (async () => {
       let key = await get(SETTING_OPENAIKEY)
       if (key) {
-        setOpenaiKey(key)
+        settings.openaiToken = key
       }
       let savedFiles = await get(SETTING_SAVED_FILES)
       if (savedFiles) {
-        setSavedFiles(savedFiles)
+        settings.savedFiles = savedFiles
       }
-      let filename = window.location.hash.substring(1)
-      if (filename == "") {
-        filename = "Untitled"
+      if (!savedFiles) {
+        await flushSavedFiles()
+        savedFiles = settings.savedFiles
       }
-      setFilename(filename)
+      if (!settings.savedFiles.has(filename)) {
+        settings.savedFiles.add(filename)
+        await flushSavedFiles()
+      }
+      settings.loaded = true
+      setSettings({...settings})
     })()
-  }, [loadedSettings])
-
-  function setFilename(filename: string) {
-    console.log(["setFilename", filename])
-    filename.replaceAll('#', '')
-    window.location.hash = filename
-    _setFilename(filename)
-    if (savedFiles.has(filename)) {
-      return
-    }
-    savedFiles.add(filename)
-    return set(SETTING_SAVED_FILES, savedFiles)
-  }
+  }, [filename])
 
   async function clickChangeAPIKey() {
     promptOpenAIAPIKey()
   }
 
   async function promptOpenAIAPIKey(msg?: string) {
-    let newKey = prompt(msg || "Enter your OpenAI API key", openaiKey)
-    if (!newKey)
+    let newKey = prompt(msg || "Enter your OpenAI API key", settings.openaiToken)
+    if (!newKey) {
       return
-    set(SETTING_OPENAIKEY, newKey)
-    setOpenaiKey(newKey)
+    }
+    settings.openaiToken = newKey
+    await set(SETTING_OPENAIKEY, newKey)
+    setSettings({...settings})
   }
   async function run() {
-    const openai = new OpenAI(openaiKey);
+    const openai = new OpenAI(settings.openaiToken);
     try {
       const gptResponse = await openai.complete({
         engine: 'text-davinci-002',
-        prompt: codeValue,
+        prompt: settings.code,
         maxTokens: 256,
         temperature: 0.7,
         topP: 1,
@@ -87,28 +92,33 @@ function Code() {
         stream: false,
         // stop: ['\n', "testing"]
       });
-      _setCodeValue(codeValue + gptResponse.data.choices[0].text);
+      settings.code += gptResponse.data.choices[0].text;
+      setSettings({...settings})
     } catch (e) {
       promptOpenAIAPIKey("Rest call to OpenAI failed. You probably need set or change your API key. Please enter your API key.")
     }
   }
-  async function promptSaveAs() {
-    let newFilename = prompt('How would you like to name you workspace', 'workspace')
-    if (!newFilename)
-      return
-    setFilename(newFilename)
+
+  function setFilename(filename: string) {
+    _navigate('/' + filename)
   }
 
+  function promptSaveAs() {
+    let newFilename = prompt("Save as", filename)
+    if (newFilename) {
+      setFilename(newFilename)
+    }
+  }
   let app = (
     <div> 
     <div>
       <select value={filename} onChange={e => setFilename(e.target.value)}>
-        {[...savedFiles.values()].map((filename) => <option value={filename} key={filename}>{filename}</option>)}
+        {[...settings.savedFiles.values()].map((filename) => <option value={filename} key={filename}>{filename}</option>)}
       </select>
-      <button onClick={promptSaveAs}>Save As</button><button onClick={clickChangeAPIKey}>{openaiKey?'Change':'Set'} OpeanAI API key</button></div>
+      <button onClick={promptSaveAs}>Save As</button><button onClick={clickChangeAPIKey}>{settings.openaiToken?'Change':'Set'} OpeanAI API key</button></div>
     <Editor autoFocus
-    value={codeValue}
-    onValueChange={code => _setCodeValue(code)}
+    value={settings.code}
+    onValueChange={code => {settings.code = code; setSettings({...settings})}}
     highlight={code => highlight(code, languages.markdown, 'markdown')}
     padding={10}
     textareaId="codeArea"
@@ -122,7 +132,7 @@ function Code() {
   <div><button onClick={run}>Run</button></div>
 </div>
   );
-  return app;
+  return settings.loaded ? app : <div>Loading...</div>;
 }
 
 export default Code;
