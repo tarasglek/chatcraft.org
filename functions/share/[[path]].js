c11f5fdbe567e46efeb0ab8bd65fa0ce576613d3
Taras Glek <taras@glek.net>
2022-10-11 20:50:55 +0300

    first func hello world

---
+++ functions/share/[[path]].js
@@ -0,0 +1,15 @@
+/** hello world cloudflare function in typescript */
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  const str = JSON.stringify(env, null, 2);
+  return new Response(`Hello, world!\n\n${str}`);
+}
