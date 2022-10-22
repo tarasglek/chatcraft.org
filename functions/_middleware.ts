6b58ed61ae1aadf13948fce1b9152cd260e81136
Taras Glek <taras@glek.net>
2022-10-22 14:12:17 +0300

    can rewrite html

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -6,21 +6,24 @@ export const onRequest = [
     try {
       // Call the next handler in the stack
       // console.log(next)
-      let n = next as Next
+      let _next = next as Next
+      let _request = request as Request
       // console.log(request.url, functionPath)
-      const url = new URL(request.url)
-      let debug = url.pathname == '/'
-      if (debug) {
+      const url = new URL(_request.url)
+      let rewrite = url.pathname == '/'
+      if (rewrite) {
+        request.headers.delete('if-none-match')
         console.log(request.headers)
       }
-      const response = await n(request, {headers: request.headers});
-      const responseText = await response.text();
+      const response = await _next(_request, {headers: _request.headers});
+      let responseText = await response.text();
       //~> "Hello from next base middleware"
       // return new Response(responseText + ` from middleware ${JSON.stringify(response.headers)}`);
       
-      if (debug)
+      if (rewrite) {
+        responseText = responseText.replace('https://private.overthinker.dev/screenshot.png', 'taras was here')
         console.log(url.pathname, functionPath, request.url, response.status, response.statusText, response.headers)
-
+      }
       if (response.status != 200) {
         return new Response(undefined, response);
       }
