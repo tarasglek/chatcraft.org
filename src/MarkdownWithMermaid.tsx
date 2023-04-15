c64cdcb1c43e3e2484dc4f287a1db0361b075e65
Taras Glek <taras@glek.net>
2023-04-15 19:10:50 +0300

    lazy load mermaid!

--- src/MarkdownWithMermaid.tsx
+++ src/MarkdownWithMermaid.tsx
@@ -5,13 +5,30 @@ interface MarkdownWithMermaidProps {
     children: string;
 }
 
-export const MarkdownWithMermaid: React.FC<MarkdownWithMermaidProps> = ({ children }) => {
-  useEffect(() => {
-    let mermaid = (window as any).mermaid
-    if (mermaid) {
-      mermaid.contentLoaded();
+function lazyLoadMermaidAndRenderThem() {
+  const scriptTags = document.getElementsByTagName('script');
+  const mermaid_js = 'mermaid.min.js';
+  for (let i = 0; i < scriptTags.length; i++) {
+    const scriptTag = scriptTags[i];
+    const src = scriptTag.getAttribute('src');
+    if (src && src.includes(mermaid_js)) {
+      let mermaid = (window as any).mermaid;
+      console.log("found mermaid script tag")
+      if (mermaid) {
+        mermaid.contentLoaded();
+        console.log("mermaid contentLoaded()")
+      }
+      return
     }
-  })
+  }
+  const scriptTag = document.createElement('script');
+  scriptTag.src = mermaid_js
+  scriptTag.onload = lazyLoadMermaidAndRenderThem;
+  document.body.appendChild(scriptTag);
+  console.log("mermaid script tag appended")
+}
+
+export const MarkdownWithMermaid: React.FC<MarkdownWithMermaidProps> = ({ children }) => {
   return (
     // <ReactMarkdown linkTarget={"_blank"} children={children}/>
     <ReactMarkdown
@@ -26,6 +43,9 @@ export const MarkdownWithMermaid: React.FC<MarkdownWithMermaidProps> = ({ childr
         )
         if (match) {
           if (match[1] === 'mermaid') {
+            useEffect(() => {
+              lazyLoadMermaidAndRenderThem()
+            })
             return (
               <>
               <div className="mermaid">
