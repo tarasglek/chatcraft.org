71ebb3ca1838780ad98c0867651d2af396838ba1
Taras Glek <taras@glek.net>
2023-04-20 14:37:38 +0300

    tiktoken argh!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   build: {
     outDir: 'build',
+    target: 'esnext',
   }
 })
