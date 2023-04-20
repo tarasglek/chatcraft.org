6d7e9497d6bc35df7278cb78143b7f53645823ab
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-20 10:32:29 -0400

    Add Chakra-UI https://chakra-ui.com/getting-started/vite-guide

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementsByClassName("chat-container")[0] as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
