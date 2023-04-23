dd461400e37e630940fe5e2173122bccfb28cab6
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-23 15:00:52 -0400

    Make prompt panel resizable by dragging

--- src/hooks/use-settings.ts
+++ src/hooks/use-settings.ts
@@ -3,6 +3,7 @@ import { useLocalStorage } from "react-use";
 const defaultSettings: Settings = {
   model: "gpt-3.5-turbo",
   enterBehaviour: "send",
+  promptPanelHeight: 120,
 };
 
 function useSettings() {
