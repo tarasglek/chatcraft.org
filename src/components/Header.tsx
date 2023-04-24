b0509ae09b9b3416f67bd1dfe69cece6a147c928
Taras Glek <taras@glek.net>
2023-04-24 22:34:18 +0300

    github icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import { BiSun, BiMoon } from "react-icons/bi";
-import { BsPersonGear } from "react-icons/bs";
+import { BsPersonGear, BsGithub } from "react-icons/bs";
 
 import PreferencesModal from "./PreferencesModal";
 
@@ -25,6 +25,16 @@ function Header() {
     >
       <ButtonGroup isAttached pr={2}>
         <IconButton
+          as="a"
+          href="https://github.com/tarasglek/chatcraft.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub Repository"
+          title="GitHub Repository"
+          icon={<BsGithub />}
+          variant="ghost"
+        />
+        <IconButton
           aria-label={useColorModeValue("Switch to Dark Mode", "Switch to Light Mode")}
           title={useColorModeValue("Switch to Dark Mode", "Switch to Light Mode")}
           icon={useColorModeValue(<BiMoon />, <BiSun />)}
@@ -38,7 +48,6 @@ function Header() {
           variant="ghost"
           onClick={onOpen}
         />
-
         <PreferencesModal isOpen={isOpen} onClose={onClose} />
       </ButtonGroup>
     </Flex>
