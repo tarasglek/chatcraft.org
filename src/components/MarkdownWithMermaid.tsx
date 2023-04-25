821f127df4343b323f174c669a219e7ac9b4542c
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-25 18:24:36 -0400

    Add code header with copy-to-clipboard

--- src/components/MarkdownWithMermaid.tsx
+++ src/components/MarkdownWithMermaid.tsx
@@ -1,16 +1,68 @@
-import { useColorModeValue } from "@chakra-ui/react";
+import type { ReactNode } from "react";
+import {
+  Flex,
+  ButtonGroup,
+  IconButton,
+  useToast,
+  useClipboard,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 import remarkMermaid from "remark-mermaidjs";
 import remarkGfm from "remark-gfm";
+import { TbCopy } from "react-icons/tb";
 
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
 // We need both a light and dark theme
 import oneDark from "react-syntax-highlighter/dist/esm/styles/prism/one-dark";
 import oneLight from "react-syntax-highlighter/dist/esm/styles/prism/one-light";
 
-interface MarkdownWithMermaidProps {
-  children: string;
+type PreHeaderProps = { children: ReactNode; code: string };
+
+function PreHeader({ children, code }: PreHeaderProps) {
+  const { onCopy } = useClipboard(code);
+  const toast = useToast();
+
+  const handleCopy = () => {
+    onCopy();
+    toast({
+      title: "Copied to Clipboard",
+      description: "Code was copied to your clipboard.",
+      status: "info",
+      duration: 3000,
+      position: "top",
+      isClosable: true,
+    });
+  };
+
+  return (
+    <>
+      <Flex
+        bg={useColorModeValue("gray.200", "gray.600")}
+        justify="end"
+        align="center"
+        borderTopLeftRadius="md"
+        borderTopRightRadius="md"
+      >
+        <ButtonGroup isAttached pr={2}>
+          <IconButton
+            size="sm"
+            aria-label="Copy to Clipboard"
+            title="Copy to Clipboard"
+            icon={<TbCopy />}
+            variant="ghost"
+            onClick={handleCopy}
+          />
+        </ButtonGroup>
+      </Flex>
+      {children}
+    </>
+  );
 }
+
+type MarkdownWithMermaidProps = {
+  children: string;
+};
 
 const MarkdownWithMermaid = ({ children }: MarkdownWithMermaidProps) => {
   return (
@@ -40,12 +92,15 @@ const MarkdownWithMermaid = ({ children }: MarkdownWithMermaidProps) => {
             prefix = <iframe className="htmlPreview" srcDoc={children as any}></iframe>;
           }
 
+          const code = String(children);
+
           return (
             <>
               {prefix}
               <SyntaxHighlighter
-                children={String(children)}
+                children={code}
                 language={language}
+                PreTag={(props) => <PreHeader {...props} code={code} />}
                 style={useColorModeValue(oneLight, oneDark)}
                 showLineNumbers={true}
                 wrapLongLines={true}
