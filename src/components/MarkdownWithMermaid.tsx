b39a5c6a7d9e7dfb4bb90bc6edae288212a3a880
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-25 14:59:08 -0400

    Switch to PrismAsyncLight

--- src/components/MarkdownWithMermaid.tsx
+++ src/components/MarkdownWithMermaid.tsx
@@ -1,23 +1,12 @@
-// TODO: switch to using proper React components for this...
 import { useColorModeValue } from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 import remarkMermaid from "remark-mermaidjs";
 import remarkGfm from "remark-gfm";
 
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-// We need a light and dark theme, see https://highlightjs.org/static/demo/
-import { atomOneLight, atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
-// Pick all languages to support
-import ts from "react-syntax-highlighter/dist/esm/languages/hljs/typescript";
-import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
-import json from "react-syntax-highlighter/dist/esm/languages/hljs/json";
-import css from "react-syntax-highlighter/dist/esm/languages/hljs/css";
-// Register all these languages
-SyntaxHighlighter.registerLanguage("typescript", ts);
-SyntaxHighlighter.registerLanguage("javascript", js);
-SyntaxHighlighter.registerLanguage("json", json);
-SyntaxHighlighter.registerLanguage("css", css);
-SyntaxHighlighter.registerLanguage("typescript", ts);
+import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
+// We need both a light and dark theme
+import oneDark from "react-syntax-highlighter/dist/esm/styles/prism/one-dark";
+import oneLight from "react-syntax-highlighter/dist/esm/styles/prism/one-light";
 
 interface MarkdownWithMermaidProps {
   children: string;
@@ -57,7 +46,7 @@ const MarkdownWithMermaid = ({ children }: MarkdownWithMermaidProps) => {
               <SyntaxHighlighter
                 children={String(children)}
                 language={language}
-                style={useColorModeValue(atomOneLight, atomOneDark)}
+                style={useColorModeValue(oneLight, oneDark)}
                 showLineNumbers={true}
                 wrapLongLines={true}
               />
