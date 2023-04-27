27446fe7116ac41911183b578a9d12c88ce1b88f
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-27 12:42:20 -0400

    PWA support via vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [
+    react(),
+    wasm(),
+    // https://vite-pwa-org.netlify.app/guide/
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ChatCraft.org",
+        short_name: "ChatCraft",
+        icons: [
+          { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   build: {
     outDir: "build",
     target: "esnext",
