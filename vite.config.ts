22aad481c100c102ae1442f1daa335ae97b6d51d
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-27 13:00:31 -0400

    Fixes for Lighthouse PWA testing

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,12 @@ export default defineConfig({
         short_name: "ChatCraft",
         icons: [
           { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-          { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
         ],
         theme_color: "#ffffff",
         background_color: "#ffffff",
