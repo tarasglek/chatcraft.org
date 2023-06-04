9dc374194a7c486ddb8275ed8276fd3d577b5f0e
David Humphrey <david.andrew.humphrey@gmail.com>
2023-06-04 14:03:11 -0400

    Use both idToken and accessToken

--- functions/api/logout.ts
+++ functions/api/logout.ts
@@ -1,4 +1,4 @@
-import { getToken, verifyToken, serializeToken } from "../token";
+import { getTokens, verifyToken, serializeToken } from "../token";
 
 interface Env {
   JWT_SECRET: string;
@@ -7,15 +7,15 @@ interface Env {
 // Log a user out by setting the max-age of the `token` cookie to 0
 export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
   const { JWT_SECRET } = env;
-  const token = getToken(request);
+  const { accessToken, idToken } = getTokens(request);
 
   // No token means user isn't logged in
-  if (!token) {
+  if (!accessToken) {
     return new Response(null, { status: 401 });
   }
 
   // A token we can't verify also means user isn't logged in
-  const payload = verifyToken(token, JWT_SECRET);
+  const payload = await verifyToken(accessToken, JWT_SECRET);
   if (!payload) {
     return new Response(null, { status: 401 });
   }
@@ -23,9 +23,10 @@ export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
   // User is logged in, expire their token/cookie
   return new Response(null, {
     status: 204,
-    headers: new Headers({
-      // Set max-age to 0 so browser deletes the cookie
-      "Set-Cookie": serializeToken(token, 0),
-    }),
+    // Set max-age to 0 so browser deletes the cookie
+    headers: new Headers([
+      ["Set-Cookie", serializeToken("access_token", accessToken, 0)],
+      ["Set-Cookie", serializeToken("id_token", idToken, 0)],
+    ]),
   });
 };
--- functions/api/share/[user].ts
+++ functions/api/share/[user].ts
@@ -1,5 +1,5 @@
 import { errorResponse } from "../../utils";
-import { serializeToken, refreshToken, getToken, verifyToken } from "../../token";
+import { serializeToken, getTokens, verifyToken } from "../../token";
 
 interface Env {
   CHATCRAFT_ORG_BUCKET: R2Bucket;
@@ -11,20 +11,20 @@ interface Env {
 export const onRequestGet: PagesFunction<Env> = async ({ request, env, params }) => {
   const { CHATCRAFT_ORG_BUCKET, JWT_SECRET } = env;
 
-  // There has to be a token in the cookies
-  const token = getToken(request);
-  if (!token) {
-    return errorResponse(403, "Missing token");
+  // There should be tokens in the cookies
+  const { accessToken, idToken } = getTokens(request);
+  if (!accessToken) {
+    return errorResponse(403, "Missing Access Token");
   }
 
   // Make sure we have a token, and that it matches the expected user
   const { user } = params;
   try {
-    const payload = await verifyToken(token, JWT_SECRET);
+    const payload = await verifyToken(accessToken, JWT_SECRET);
 
     // Make sure this is the same username as the user who owns this token
     if (payload?.sub !== params.user) {
-      return errorResponse(403, "Token does not match user");
+      return errorResponse(403, "Access Token does not match user");
     }
   } catch (err) {
     return errorResponse(400, err.message);
@@ -39,17 +39,13 @@ export const onRequestGet: PagesFunction<Env> = async ({ request, env, params })
       url: `https://chatcraft.org/c/${key}`,
     }));
 
-    // Update token/cookie to further delay expiry
-    const chatCraftToken = await refreshToken(token, JWT_SECRET);
-    if (!chatCraftToken) {
-      throw new Error("Unable to refresh token");
-    }
-
     return new Response(JSON.stringify(body), {
       status: 200,
-      headers: new Headers({
-        "Set-Cookie": serializeToken(chatCraftToken),
-      }),
+      // Update cookies to further delay expiry
+      headers: new Headers([
+        ["Set-Cookie", serializeToken("access_token", accessToken)],
+        ["Set-Cookie", serializeToken("id_token", idToken)],
+      ]),
     });
   } catch (err) {
     console.error(err);
