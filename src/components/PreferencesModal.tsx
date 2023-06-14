743fb1b6158490894b0ebe0b7947331533b3c706
Taras Glek <taras@glek.net>
2023-06-14 08:12:13 +0200

    misc

--- src/components/PreferencesModal.tsx
+++ src/components/PreferencesModal.tsx
@@ -62,26 +62,11 @@ function PreferencesModal({ isOpen, onClose, finalFocusRef }: PreferencesModalPr
   }, []);
 
   useEffect(() => {
-    async function fetchModels() {
-      const response = await fetch("https://api.openai.com/v1/models", {
-        headers: {
-          Authorization: `Bearer ${settings.apiKey}`,
-        },
-      });
-      const data = await response.json();
-      setModels(
-        data.data
-          // Hide all pinned models (visual noise) except gpt-3.5-turbo-0613 as that wont be default till June 27 :(
-          .filter(
-            (model: any) =>
-              model.id.includes("gpt") &&
-              (model.id == "gpt-3.5-turbo-0613" || !/\d{4}$/.test(model.id))
-          )
-          .map((model: any) => new ChatCraftModel(`OpenAI/${model.id}`))
-      );
+    async function fetchModels(apiKey: string) {
+      setModels(await ChatCraftModel.fetchModels(apiKey));
     }
-    if (isOpen) {
-      fetchModels();
+    if (isOpen && settings.apiKey) {
+      fetchModels(settings.apiKey);
     }
   }, [settings.apiKey, isOpen]);
 
