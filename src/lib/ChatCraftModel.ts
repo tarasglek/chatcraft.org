6339984c5cdc8b273a703de803b5820d7e13ba13
Taras Glek <taras@glek.net>
2023-06-14 08:12:13 +0200

    misc

--- src/lib/ChatCraftModel.ts
+++ src/lib/ChatCraftModel.ts
@@ -27,12 +27,31 @@ export class ChatCraftModel {
   get prettyModel(): string {
     console.log(this.modelId);
     switch (this.modelId) {
-      case "OpenAI/gpt-3.5-turbo":
+      case "OpenAI/gpt-3.5-turbo-0613":
         return "ChatGPT";
+      case "OpenAI/gpt-3.5-turbo":
+        return "ChatGPT-0301";
       case "OpenAI/gpt-4":
         return "GPT-4";
       default:
         return `${this.vendor} ${this.model}`;
     }
+  }
+  static async fetchModels(apiKey: string): Promise<ChatCraftModel[]> {
+    const response = await fetch("https://api.openai.com/v1/models", {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+    const data = await response.json();
+    // Hide all pinned models (visual noise) except gpt-3.5-turbo-0613 as that wont be default till June 27 :(
+    const ret = data.data
+      .filter(
+        (model: any) =>
+          model.id.includes("gpt") && (model.id == "gpt-3.5-turbo-0613" || !/\d{4}$/.test(model.id))
+      )
+      .map((model: any) => new ChatCraftModel(`OpenAI/${model.id}`));
+    console.log(ret);
+    return ret;
   }
 }
