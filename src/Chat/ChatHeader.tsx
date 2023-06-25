f371489ba696d00f9c00901531064b3abf31804e
David Humphrey <david.andrew.humphrey@gmail.com>
2023-06-24 21:34:57 -0400

    Add optional chat title to db, model, and UI

--- src/Chat/ChatHeader.tsx
+++ src/Chat/ChatHeader.tsx
@@ -1,22 +1,30 @@
+import { FormEvent, useEffect, useState } from "react";
 import {
+  Box,
+  Button,
+  ButtonGroup,
   Card,
   CardBody,
+  CardFooter,
   Flex,
   Heading,
   IconButton,
+  Input,
   Link,
   Menu,
   MenuButton,
   MenuDivider,
   MenuItem,
   MenuList,
+  Text,
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 import { Link as ReactRouterLink, useFetcher } from "react-router-dom";
 import { MdOutlineChatBubbleOutline } from "react-icons/md";
 import { TbDots } from "react-icons/tb";
-import { useCopyToClipboard } from "react-use";
+import { AiOutlineEdit } from "react-icons/ai";
+import { useCopyToClipboard, useKey } from "react-use";
 
 import { ChatCraftChat } from "../lib/ChatCraftChat";
 import { download, formatDate } from "../lib/utils";
@@ -33,6 +41,13 @@ function ChatHeader({ chat }: ChatHeaderProps) {
   const toast = useToast();
   const fetcher = useFetcher();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isEditing, setIsEditing] = useState(false);
+
+  useKey("Escape", () => setIsEditing(false), { event: "keydown" }, [setIsEditing]);
+
+  useEffect(() => {
+    setIsEditing(false);
+  }, [chat.id]);
 
   const handleCopyChatClick = () => {
     const text = chat.toMarkdown();
@@ -78,6 +93,31 @@ function ChatHeader({ chat }: ChatHeaderProps) {
     fetcher.submit({}, { method: "post", action: `/c/${chat.id}/delete` });
   };
 
+  const handleSaveTitle = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = new FormData(e.target as HTMLFormElement);
+    const title = data.get("title");
+    if (typeof title !== "string") {
+      return;
+    }
+
+    chat.title = title;
+    chat
+      .update()
+      .catch((err) => {
+        console.warn("Unable to update title for chat", err);
+        toast({
+          title: `Error Updating Chat`,
+          description: "message" in err ? err.message : undefined,
+          status: "error",
+          position: "top",
+          isClosable: true,
+        });
+      })
+      .finally(() => setIsEditing(false));
+  };
+
   return (
     <>
       <Card
@@ -92,7 +132,7 @@ function ChatHeader({ chat }: ChatHeaderProps) {
         }}
         mt={2}
       >
-        <CardBody>
+        <CardBody pb={0}>
           <Flex justify="space-between" align="center">
             <Heading as="h2" fontSize="md">
               <Link as={ReactRouterLink} to={`/c/${chat.id}`}>
@@ -136,6 +176,54 @@ function ChatHeader({ chat }: ChatHeaderProps) {
             </Menu>
           </Flex>
         </CardBody>
+        <CardFooter pt={0}>
+          <Box w="100%">
+            {isEditing ? (
+              <form onSubmit={handleSaveTitle}>
+                <Flex align="center" gap={2}>
+                  <Input
+                    flex={1}
+                    defaultValue={chat.title}
+                    type="text"
+                    name="title"
+                    bg="white"
+                    _dark={{ bg: "gray.700" }}
+                    size="sm"
+                    w="100%"
+                    autoFocus={true}
+                    placeholder="Chat Title"
+                  />
+                  <ButtonGroup>
+                    <Button variant="outline" size="sm" onClick={() => setIsEditing(false)}>
+                      Cancel
+                    </Button>
+                    <Button size="sm" type="submit">
+                      Save
+                    </Button>
+                  </ButtonGroup>
+                </Flex>
+              </form>
+            ) : chat.title ? (
+              <Flex align="center" gap={2} maxW="100%">
+                <Text fontSize="sm" color="gray.500" _dark={{ color: "gray.400" }} ml={6}>
+                  {chat.title}
+                </Text>
+                <IconButton
+                  variant="ghost"
+                  size="sm"
+                  icon={<AiOutlineEdit />}
+                  aria-label="Edit title"
+                  title="Edit title"
+                  onClick={() => setIsEditing(true)}
+                />
+              </Flex>
+            ) : (
+              <Button size="sm" variant="ghost" onClick={() => setIsEditing(!isEditing)}>
+                Add a title...
+              </Button>
+            )}
+          </Box>
+        </CardFooter>
       </Card>
 
       <ShareModal chat={chat} isOpen={isOpen} onClose={onClose} />
