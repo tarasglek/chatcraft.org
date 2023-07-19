c856537ebcd4a403a9dc6ff89459aca57283ea47
tarasglek <taras@thegp.com>
2023-07-19 09:55:44 +0000

    replit support

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+    deps = [
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-18_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+        pkgs.miniserve
+    ];
+}
