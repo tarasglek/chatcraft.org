13b6ac145e8f92b0608a5f33bf9d2b94326a7a7f
Taras Glek <taras@glek.net>
2023-07-22 11:14:08 +0000

    bun

--- replit.nix
+++ replit.nix
@@ -1,10 +1,12 @@
 { pkgs }: {
     deps = [
+        pkgs.unzip
         pkgs.esbuild
         pkgs.nodejs-18_x
 
         pkgs.miniserve
         pkgs.time
         pkgs.pstree
+      pkgs.unzip
     ];
 }
