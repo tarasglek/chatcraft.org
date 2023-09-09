4f11aec9c8862273ab3697367d67d3989da21d11
Taras Glek <taras@glek.net>
2023-09-09 08:32:21 +0300

    can record audio into a blob on chrome

--- src/lib/speech-recognition.ts
+++ src/lib/speech-recognition.ts
@@ -1,14 +1,50 @@
+type RecordingInProgress = () => Promise<Blob>;
+/**
+ * Openai supports: m4a mp3 webm mp4 mpga wav mpeg
+ * of these audio/webm is supported by chrome, firefox
+ * and audio/mp3 is supported by safari
+ */
+async function startRecording(): Promise<RecordingInProgress> {
+  const recordedChunks: BlobPart[] = [];
+  let stopRecordingResolve: (value: Blob) => void;
+  const audioDataPromise = new Promise<Blob>((resolve) => {
+    stopRecordingResolve = resolve;
+  });
+  const mimeType = "audio/webm";
+  const stream: MediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
+
+  mediaRecorder.ondataavailable = function (e) {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data);
+    }
+  };
+
+  mediaRecorder.onstop = function () {
+    const blob = new Blob(recordedChunks, {
+      type: mimeType,
+    });
+    stopRecordingResolve(blob);
+  };
+
+  mediaRecorder.start();
+
+  return () => {
+    mediaRecorder.stop();
+    return audioDataPromise;
+  };
+}
+
 export class SpeechRecognition {
-  private _isRecording: boolean;
   private _isCancelled: boolean;
+  private _recording = null as RecordingInProgress | null;
 
   constructor() {
-    this._isRecording = false;
     this._isCancelled = false;
   }
 
   get isRecording() {
-    return this._isRecording;
+    return this._recording !== null;
   }
 
   get isCancelled() {
@@ -16,7 +52,7 @@ export class SpeechRecognition {
   }
 
   async start() {
-    if (this._isRecording) {
+    if (this.isRecording) {
       throw new Error("Recording already started");
     }
 
@@ -25,7 +61,7 @@ export class SpeechRecognition {
     }
 
     console.log("Recording started...");
-    this._isRecording = true;
+    this._recording = await startRecording();
   }
 
   async stop() {
@@ -34,21 +70,26 @@ export class SpeechRecognition {
       return;
     }
 
-    if (!this._isRecording) {
+    const stopFunc = this._recording;
+    if (!stopFunc) {
       throw new Error("No recording in progress");
     }
 
+    const result = await stopFunc();
+    console.log("recorded", result.type, result.size);
+
     console.log("Recording stopped");
-    this._isRecording = false;
+    this._recording = null;
   }
 
   async cancel() {
-    if (!this._isRecording) {
+    const stopFunc = this._recording;
+    if (!stopFunc) {
       throw new Error("No recording in progress");
     }
-
+    stopFunc();
+    this._recording = null;
     console.log("Recording cancelled.");
     this._isCancelled = true;
-    this._isRecording = false;
   }
 }
