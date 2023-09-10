b25651f6367b1f9a7116580d88a095d28c0d1b5e
Taras Glek <taras@glek.net>
2023-09-10 14:23:41 +0300

    rewrote state machine

--- src/lib/speech-recognition.ts
+++ src/lib/speech-recognition.ts
@@ -1,123 +1,113 @@
 import { transcribe } from "./ai";
 
-type RecordingInProgress = () => Promise<File>;
-/**
- * Openai supports: m4a mp3 webm mp4 mpga wav mpeg
- * of these audio/webm is supported by chrome, firefox
- * and audio/mp3 is supported by safari
- */
-async function startRecording(): Promise<RecordingInProgress> {
-  const recordedChunks: BlobPart[] = [];
-  let stopRecordingResolve: (value: File) => void;
-  const audioDataPromise = new Promise<File>((resolve) => {
-    stopRecordingResolve = resolve;
-  });
-
-  let stream: MediaStream;
-  try {
-    stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  } catch (e: any) {
-    if (e.name === "NotAllowedError") {
-      throw new Error("Recording permission denied");
-    }
-    throw e;
-  }
-
-  let mimeType = "audio/webm";
-  let mediaRecorder: MediaRecorder;
-  // Handle Safari not supporting webm
-  try {
-    mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
-  } catch (e: any) {
-    if (e.name === "NotSupportedError") {
-      mimeType = "audio/mp3";
-      mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
-    } else {
-      throw e;
-    }
-  }
-
-  mediaRecorder.ondataavailable = function (e) {
-    if (e.data.size > 0) {
-      recordedChunks.push(e.data);
-    }
-  };
-
-  mediaRecorder.onstop = function () {
-    const file = new File(recordedChunks, mimeType.replace("/", "."), {
-      type: mimeType,
-    });
-    stopRecordingResolve(file);
-  };
-
-  mediaRecorder.start();
-
-  return () => {
-    mediaRecorder.stop();
-    return audioDataPromise;
-  };
-}
-
 export class SpeechRecognition {
-  private _isCancelled: boolean;
-  private _recording = null as RecordingInProgress | null;
-
-  constructor() {
-    this._isCancelled = false;
-  }
+  private _recordingPromise: Promise<File> | null = null;
+  private _mediaRecorder: MediaRecorder | null = null;
 
   get isRecording() {
-    return this._recording !== null;
+    return this._recordingPromise !== null;
   }
 
-  get isCancelled() {
-    return this._isCancelled;
-  }
-
-  async start() {
+  async start(): Promise<void> {
     if (this.isRecording) {
       throw new Error("Recording already started");
     }
 
-    if (this._isCancelled) {
-      throw new Error("Recording cancelled");
-    }
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const self = this;
+    let recordingPromiseResolve: (value: File) => void;
+    const audioDataPromise = new Promise<File>((resolve) => {
+      recordingPromiseResolve = resolve;
+    });
 
-    console.log("Recording started...");
-    this._recording = await startRecording();
+    // wrap this up into a promise that runs after we return callback so we can cancel while accepting/rejecting permissions
+    const initPromise = (async () => {
+      const recordedChunks: BlobPart[] = [];
+
+      let stream: MediaStream;
+      try {
+        stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      } catch (e: any) {
+        if (e.name === "NotAllowedError") {
+          throw new Error("Recording permission denied");
+        }
+        throw e;
+      }
+
+      let mimeType = "audio/webm";
+      // Handle Safari not supporting webm
+      try {
+        this._mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
+      } catch (e: any) {
+        if (e.name === "NotSupportedError") {
+          mimeType = "audio/mp3";
+          this._mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
+        } else {
+          throw e;
+        }
+      }
+
+      this._mediaRecorder.ondataavailable = function (e) {
+        if (!self.isRecording) {
+          // Recording cancelled
+          if (self._mediaRecorder) {
+            self._mediaRecorder.stop();
+          }
+          stream.getTracks().forEach((track) => track.stop());
+          return;
+        }
+        if (e.data.size > 0) {
+          recordedChunks.push(e.data);
+        }
+      };
+
+      this._mediaRecorder.onstop = function () {
+        if (!self.isRecording) {
+          return;
+        }
+        const fname = mimeType.replace("/", ".");
+        const file = new File(recordedChunks, fname, {
+          type: mimeType,
+        });
+        recordingPromiseResolve(file);
+      };
+
+      if (this.isRecording) {
+        this._mediaRecorder.start();
+      } else {
+        this._mediaRecorder = null;
+      }
+    })();
+    this._recordingPromise = initPromise.then(() => audioDataPromise);
+    await initPromise;
   }
 
   async stop(): Promise<string | null> {
-    if (this._isCancelled) {
-      // Recording already stopped via `.cancel()`
-      console.warn("Recording cancelled");
+    const recordingPromise = this._recordingPromise;
+    const mediaRecorder = this._mediaRecorder;
+    if (!mediaRecorder && recordingPromise) {
+      // If we have a recording promise but no mediaRecorder, means we got prompted for permissions and had to abort recording to accept/reject
+      this._recordingPromise = null;
+      return null;
+    } else if (mediaRecorder) {
+      mediaRecorder.stop();
+      this._mediaRecorder = null;
+    }
+    if (!recordingPromise) {
       return null;
     }
-
-    const stopFunc = this._recording;
-    if (!stopFunc) {
-      throw new Error("No recording in progress");
-    }
-
-    const filePromise = stopFunc();
-    this._recording = null;
-
-    const file = await filePromise;
+    const file = await recordingPromise;
+    this._recordingPromise = null;
     // log stats
-    console.log(`Recording stopped. File size: ${file.size} bytes. File type: ${file.type}`);
     const transcription = await transcribe(file);
-
     return transcription;
   }
 
   async cancel() {
-    const stopFunc = this._recording;
-    if (!stopFunc) {
+    if (!this.isRecording) {
       throw new Error("No recording in progress");
     }
-    stopFunc();
-    this._recording = null;
-    console.log("Recording cancelled.");
-    this._isCancelled = true;
+    this._recordingPromise = null;
+    this.stop();
   }
 }
