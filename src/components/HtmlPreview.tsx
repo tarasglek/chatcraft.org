56c7e0e2ccda61b8ee8ec49949f26b7b7ecc430e
mingming-ma <133393905+mingming-ma@users.noreply.github.com>
2023-10-26 12:12:59 -0400

    don't use else after a return

--- src/components/HtmlPreview.tsx
+++ src/components/HtmlPreview.tsx
@@ -15,15 +15,14 @@ const HtmlPreview = ({ children, isLoading = false }: HtmlPreviewProps) => {
   const url = useMemo(() => {
     if (isLoading) {
       return "about:blank";
-    } else {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(String(children), "text/html");
-      const scriptElement = document.createElement("script");
-      scriptElement.src = new URL("/js/iframeResizer.contentWindow.min.js", import.meta.url).href;
-      doc.body.appendChild(scriptElement);
-      const html = `<!DOCTYPE html>${doc.documentElement.innerHTML}`;
-      return toUrl(html);
     }
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(String(children), "text/html");
+    const scriptElement = document.createElement("script");
+    scriptElement.src = new URL("/js/iframeResizer.contentWindow.min.js", import.meta.url).href;
+    doc.body.appendChild(scriptElement);
+    const html = `<!DOCTYPE html>${doc.documentElement.innerHTML}`;
+    return toUrl(html);
   }, [children, isLoading]);
 
   return (
