6f052d1182c16d485b151d9750abeffe6bd52cd7
mingming-ma <133393905+mingming-ma@users.noreply.github.com>
2023-11-09 13:18:09 -0500

    rename image_url to image and remove debug log

--- src/Chat/ChatBase.tsx
+++ src/Chat/ChatBase.tsx
@@ -141,7 +141,7 @@ function ChatBase({ chat }: ChatBaseProps) {
 
   // Handle prompt form submission
   const onPrompt = useCallback(
-    async (prompt?: string, image_url?: string[]) => {
+    async (prompt?: string, image?: string[]) => {
       setLoading(true);
 
       // Special-case for "help", to invoke /help command
@@ -184,7 +184,7 @@ function ChatBase({ chat }: ChatBaseProps) {
         // If the prompt text exist, package it up as a human message and add to the chat
         if (prompt) {
           // Add this prompt message to the chat
-          promptMessage = new ChatCraftHumanMessage({ text: prompt, image_url, user });
+          promptMessage = new ChatCraftHumanMessage({ text: prompt, image, user });
           await chat.addMessage(promptMessage);
         } else {
           // If there isn't any prompt text, see if the final message in the chat was a human
@@ -220,7 +220,6 @@ function ChatBase({ chat }: ChatBaseProps) {
 
         // NOTE: we strip out the ChatCraft App messages before sending to OpenAI.
         const messages = chat.messages({ includeAppMessages: false });
-        console.log(messages);
         const response = await callChatApi(messages, {
           functions,
           functionToCall,
