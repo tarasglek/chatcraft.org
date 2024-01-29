86732543287e0069dae3891c1527bb2c3eb58de6
Rachit1313 <Rachitchawla33@gmail.com>
2024-01-28 20:36:22 -0500

    setup css for submenus and menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Menu, MenuButton, MenuItem, SubMenu } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue, IconButton, useTheme } from "@chakra-ui/react";
+import { TbDots } from "react-icons/tb";
 
 export interface MenuItemProps {
   label: string;
@@ -14,16 +15,30 @@ interface ChakraMenuProps {
 }
 
 const ChakraMenu: React.FC<ChakraMenuProps> = ({ items }) => {
+  const theme = useTheme();
+  const menuButtonColor = useColorModeValue(theme.colors.black, theme.colors.white);
+  const menuListBg = useColorModeValue(theme.colors.white, theme.colors.gray[700]);
+  const menuListBorderColor = useColorModeValue(theme.colors.gray[200], theme.colors.gray[600]);
   const renderMenuItem = (item: MenuItemProps) => {
+    const menuItemStyles = {
+      color: menuButtonColor,
+      backgroundColor: menuListBg,
+      borderColor: menuListBorderColor,
+    };
     if (item.subItems) {
       return (
-        <SubMenu label={item.label} key={item.label}>
+        <SubMenu
+          label={item.label}
+          key={item.label}
+          menuStyle={{ backgroundColor: menuListBg }}
+          itemProps={{ style: menuItemStyles }}
+        >
           {item.subItems.map((subItem) => renderMenuItem(subItem))}
         </SubMenu>
       );
     } else {
       return (
-        <MenuItem key={item.label} onClick={item.onClick}>
+        <MenuItem key={item.label} onClick={item.onClick} style={menuItemStyles}>
           {item.label}
         </MenuItem>
       );
@@ -34,19 +49,16 @@ const ChakraMenu: React.FC<ChakraMenuProps> = ({ items }) => {
     <Box>
       <Menu
         menuButton={
-          <MenuButton
-            style={{
-              borderRadius: "4px",
-              backgroundColor: "#319795",
-              color: "white",
-              padding: "6px 24px",
-              fontSize: "16px",
-              fontWeight: "600",
-            }}
-          >
-            Open Menu
+          <MenuButton>
+            <IconButton icon={<TbDots />} variant="ghost" size="sm" aria-label="Options" />
           </MenuButton>
         }
+        menuStyle={{
+          borderColor: menuListBorderColor,
+          backgroundColor: menuListBg,
+          color: menuButtonColor,
+          boxShadow: theme.shadows.md,
+        }}
       >
         {items.map((item) => renderMenuItem(item))}
       </Menu>
