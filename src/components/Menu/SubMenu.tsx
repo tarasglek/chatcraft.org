cb50ed7fb8b3dc1a818f0db45aad7743f7300945
Rachit Chawla <58596563+Rachit1313@users.noreply.github.com>
2024-02-05 22:33:13 +0000

    Fix SubMenu component to expose all the default react props

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -1,19 +1,11 @@
 import { Box } from "@chakra-ui/react";
-import { SubMenu as ReactMenuSubMenu } from "@szhsin/react-menu";
+import { SubMenu as ReactMenuSubMenu, type SubMenuProps } from "@szhsin/react-menu";
 import React from "react";
 
-export interface SubMenuProps {
-  label: string;
-  children: React.ReactNode;
-  className?: string;
-}
-
-const SubMenu: React.FC<SubMenuProps> = ({ label, children, className }) => {
+const SubMenu: React.FC<SubMenuProps> = (props) => {
   return (
     <Box>
-      <ReactMenuSubMenu label={label} className={className}>
-        {children}
-      </ReactMenuSubMenu>
+      <ReactMenuSubMenu {...props}>{props.children}</ReactMenuSubMenu>
     </Box>
   );
 };
