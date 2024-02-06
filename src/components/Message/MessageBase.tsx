9a35820ba21fa5245a9d594b0937984fd5e8ae6d
mingming-ma <133393905+mingming-ma@users.noreply.github.com>
2024-02-06 00:03:32 -0500

    rebase on main

--- src/components/Message/MessageBase.tsx
+++ src/components/Message/MessageBase.tsx
@@ -53,7 +53,6 @@ import { ChatCraftModel } from "../../lib/ChatCraftModel";
 import { useModels } from "../../hooks/use-models";
 import { useSettings } from "../../hooks/use-settings";
 import { useAlert } from "../../hooks/use-alert";
-import ImageModal from "../ImageModal";
 
 // Styles for the message text are defined in CSS vs. Chakra-UI
 import "./Message.css";
@@ -102,7 +101,7 @@ function MessageBase({
   disableFork,
   disableEdit,
 }: MessageBaseProps) {
-  const { id, date, text, image } = message;
+  const { id, date, text } = message;
   const { models } = useModels();
   const { onCopy } = useClipboard(text);
   const { info, error } = useAlert();
@@ -112,9 +111,6 @@ function MessageBase({
   const isNarrowScreen = useMobileBreakpoint();
   const messageForm = useRef<HTMLFormElement>(null);
   const meta = useMemo(getMetaKey, []);
-  const [imageModalOpen, setImageModalOpen] = useState<boolean>(false);
-  const [selectedImage, setSelectedImage] = useState<string>("");
-
   const { isOpen, onToggle: originalOnToggle } = useDisclosure();
   const isLongMessage = text.length > 5000;
   const displaySummaryText = !isOpen && (summaryText || isLongMessage);
@@ -222,12 +218,6 @@ function MessageBase({
     },
     [message, onResubmitClick, chatId, error, onEditingChange]
   );
-
-  const openModalWithImage = (imageSrc: string) => {
-    setSelectedImage(imageSrc);
-    setImageModalOpen(true);
-  };
-  const closeModal = () => setImageModalOpen(false);
 
   return (
     <Box
@@ -419,15 +409,6 @@ function MessageBase({
                 </form>
               ) : (
                 <>
-                  {image.map((image, index) => (
-                    <Image
-                      key={`${id}-${index}`}
-                      src={image}
-                      alt={`Images# ${index}`}
-                      width="100%"
-                      onClick={() => openModalWithImage(image)}
-                    />
-                  ))}
                   <Markdown
                     previewCode={!hidePreviews && !displaySummaryText}
                     isLoading={isLoading}
@@ -444,7 +425,6 @@ function MessageBase({
                 </>
               )}
             </Box>
-            <ImageModal isOpen={imageModalOpen} onClose={closeModal} imageSrc={selectedImage} />
           </Flex>
         </CardBody>
         {footer && <CardFooter py={2}>{footer}</CardFooter>}
