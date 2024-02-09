f4acb1c5494014a3e8f285c06a35790de9cdd217
Amnish <amnishsingh04@gmail.com>
2024-02-09 18:18:31 -0500

    Pack Sidebar in its own module

---
+++ src/components/Sidebar/SidebarDesktop.tsx
@@ -0,0 +1,42 @@
+import { Box, keyframes } from "@chakra-ui/react";
+import SidebarContent, { SidebarContentProps } from "./SidebarContent";
+
+type SidebarDesktopProps = SidebarContentProps & {
+  isSidebarVisible: boolean;
+};
+
+function SidebarDesktop({ isSidebarVisible, selectedChat, selectedFunction }: SidebarDesktopProps) {
+  const sidebarOpenAnimationKeyframes = keyframes`
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  `;
+
+  const sidebarCloseAnimationKeyframes = keyframes`
+    from {
+      transform: scaleX(1);
+    }
+
+    to {
+      transform: scaleX(0);
+    }
+  `;
+
+  const sidebarOpenAnimation = `${sidebarOpenAnimationKeyframes} 500ms ease-in-out forwards`;
+  const sidebarCloseAnimation = `${sidebarCloseAnimationKeyframes} 100ms ease-in-out forwards`;
+
+  return (
+    <Box
+      transformOrigin={"left"}
+      animation={isSidebarVisible ? sidebarOpenAnimation : sidebarCloseAnimation}
+    >
+      <SidebarContent selectedChat={selectedChat} selectedFunction={selectedFunction} />
+    </Box>
+  );
+}
+
+export default SidebarDesktop;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,19 @@
+import useMobileBreakpoint from "../../hooks/use-mobile-breakpoint";
+import { ChatCraftChat } from "../../lib/ChatCraftChat";
+import { ChatCraftFunction } from "../../lib/ChatCraftFunction";
+import SidebarDesktop from "./SidebarDesktop";
+import SidebarMobile from "./SidebarMobile";
+
+export type SidebarProps = {
+  searchText?: string; // For the search field in mobile sidebar
+  selectedChat?: ChatCraftChat;
+  selectedFunction?: ChatCraftFunction;
+  isSidebarVisible: boolean;
+  handleToggleSidebarVisible: () => void;
+};
+
+export default function Sidebar(props: SidebarProps) {
+  const isMobile = useMobileBreakpoint();
+
+  return isMobile ? <SidebarMobile {...props} /> : <SidebarDesktop {...props} />;
+}
