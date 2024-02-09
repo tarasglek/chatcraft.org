32fbb5a5346435e3dac98d4fc1febd334b4206c8
David Bronaugh <dbronaugh@gmail.com>
2024-02-09 00:52:21 -0800

    Add variable declaration for preElement, fix forEach

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -40,10 +40,14 @@ function generateSharingHTML(chat: ChatCraftChat, user: User) {
 
   mainElement.innerHTML = "";
   // Use html-escaped yaml for payload
+<<<<<<< HEAD
   preElement = document.createElement("pre");
+=======
+  const preElement = document.createElement("pre");
+>>>>>>> 96a3626 (Fix typo with setText vs textContent, fix forEach statement)
   preElement.textContent = chat.toYAML();
   mainElement.append(preElement);
-  mainElement.attributes.forEach((x) => mainElement.attributes.removeNamedItem(x));
+  [...mainElement.attributes].forEach((attr:Attr) => mainElement.attributes.removeNamedItem(attr.name));
 
   const lastMessage = chat.messages().pop();
   const lastMessageText = lastMessage?.text;
