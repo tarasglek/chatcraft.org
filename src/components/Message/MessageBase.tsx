2208ad2f70705dbd56b9f6e17144d1ae707615bd
Rachit Chawla <58596563+Rachit1313@users.noreply.github.com>
2024-02-16 18:03:28 -0500

    show delete submenu only when there is more than 1 delete option (#461)
    * show delete submenu only when there is more than 1 delete option
    
    * Simplified expression to use ternary

--- src/components/Message/MessageBase.tsx
+++ src/components/Message/MessageBase.tsx
@@ -416,15 +416,8 @@ function MessageBase({
                 )}
 
                 {shouldShowDeleteMenu && (
-                  <SubMenu label="Delete" className="delete-button">
-                    {onDeleteBeforeClick && (
-                      <MenuItem
-                        label="Delete Messages Before"
-                        onClick={onDeleteBeforeClick}
-                        className="delete-button"
-                      />
-                    )}
-                    {onDeleteClick && (
+                  <>
+                    {onDeleteClick && !onDeleteBeforeClick && !onDeleteAfterClick ? (
                       <MenuItem
                         label="Delete Message"
                         onClick={onDeleteClick}
@@ -438,15 +431,40 @@ function MessageBase({
                           />
                         }
                       />
+                    ) : (
+                      <SubMenu label="Delete" className="delete-button">
+                        {onDeleteBeforeClick && (
+                          <MenuItem
+                            label="Delete Messages Before"
+                            onClick={onDeleteBeforeClick}
+                            className="delete-button"
+                          />
+                        )}
+                        {onDeleteClick && (
+                          <MenuItem
+                            label="Delete Message"
+                            onClick={onDeleteClick}
+                            className="delete-button"
+                            icon={
+                              <IconButton
+                                variant="ghost"
+                                icon={<TbTrash color="red" />}
+                                aria-label="Delete message"
+                                title="Delete message"
+                              />
+                            }
+                          />
+                        )}
+                        {onDeleteAfterClick && (
+                          <MenuItem
+                            label="Delete Messages After"
+                            onClick={onDeleteAfterClick}
+                            className="delete-button"
+                          />
+                        )}
+                      </SubMenu>
                     )}
-                    {onDeleteAfterClick && (
-                      <MenuItem
-                        label="Delete Messages After"
-                        onClick={onDeleteAfterClick}
-                        className="delete-button"
-                      />
-                    )}
-                  </SubMenu>
+                  </>
                 )}
               </Menu>
             </Flex>
