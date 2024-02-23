28e8623b93847eff6440f8c37db76f5eeccb0c8b
Amnish Singh Arora <amnishsingh04@gmail.com>
2024-02-23 00:55:07 -0500

    Make audio player queue global and allow stopping tts

--- src/hooks/use-audio-player.tsx
+++ src/hooks/use-audio-player.tsx
@@ -1,12 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext, useContext, ReactNode, FC } from "react";
 
-const useAudioPlayer = () => {
+type AudioPlayerContextType = {
+  addToAudioQueue: (audioClipUri: Promise<string>) => void;
+  clearAudioQueue: () => void;
+};
+
+const AudioPlayerContext = createContext<AudioPlayerContextType>({
+  addToAudioQueue: () => {},
+  clearAudioQueue: () => {},
+});
+
+const useAudioPlayer = () => useContext(AudioPlayerContext);
+
+export const AudioPlayerProvider: FC<{ children: ReactNode }> = ({ children }) => {
   // We are managing promises of audio urls instead of directly storing strings
   // because there is no guarantee when openai tts api finishes processing and resolves a specific url
   // For more info, check this comment:
   // https://github.com/tarasglek/chatcraft.org/pull/357#discussion_r1473470003
   const [queue, setQueue] = useState<Promise<string>[]>([]);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [currentAudioUrl, setCurrentAudioUrl] = useState<string | null>();
 
   useEffect(() => {
     if (!isPlaying && queue.length > 0) {
@@ -19,18 +32,31 @@ const useAudioPlayer = () => {
     const audioUrl: string = await audioClipUri;
     const audio = new Audio(audioUrl);
     audio.onended = () => {
+      setCurrentAudioUrl(null);
       URL.revokeObjectURL(audioUrl);
       setQueue((oldQueue) => oldQueue.slice(1));
       setIsPlaying(false);
     };
     audio.play();
+    setCurrentAudioUrl(audioUrl);
   };
 
   const addToAudioQueue = (audioClipUri: Promise<string>) => {
     setQueue((oldQueue) => [...oldQueue, audioClipUri]);
   };
 
-  return { addToAudioQueue };
+  const clearAudioQueue = () => {
+    if (currentAudioUrl) {
+      URL.revokeObjectURL(currentAudioUrl);
+      setCurrentAudioUrl(null);
+    }
+
+    setQueue((oldQueue) => oldQueue.splice(0));
+  };
+
+  const value = { addToAudioQueue, clearAudioQueue };
+
+  return <AudioPlayerContext.Provider value={value}>{children}</AudioPlayerContext.Provider>;
 };
 
 export default useAudioPlayer;
