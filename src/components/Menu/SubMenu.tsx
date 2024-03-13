fe885fe418f30d6bf0933419e735e17177297fdb
Amnish <amnishsingh04@gmail.com>
2024-03-13 12:45:35 -0400

    Cap submenu maxHeight to 50vh to prevent weird autoscroll

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -1,18 +1,11 @@
-import { Box } from "@chakra-ui/react";
 import { SubMenu as ReactMenuSubMenu, type SubMenuProps } from "@szhsin/react-menu";
 import React from "react";
 
 const SubMenu: React.FC<SubMenuProps> = (props) => {
   return (
-    <Box>
-      <ReactMenuSubMenu
-        menuStyle={{ maxHeight: "50vw", overflow: "auto" }}
-        align="center"
-        {...props}
-      >
-        {props.children}
-      </ReactMenuSubMenu>
-    </Box>
+    <ReactMenuSubMenu menuStyle={{ maxHeight: "50vh", overflow: "auto" }} align="center" {...props}>
+      {props.children}
+    </ReactMenuSubMenu>
   );
 };
 
