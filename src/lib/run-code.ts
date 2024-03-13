8e4749f36b66df7e644a33fb55d3d4e73f6f1b44
YumeiWang <yumei.wang@mohawkcollege.ca>
2024-03-12 23:32:31 -0400

    Add Ruby in run-code

--- src/lib/run-code.ts
+++ src/lib/run-code.ts
@@ -12,7 +12,13 @@ globalThis.__esbuildWasmLoaded = false;
 const supportedJS = ["js", "javascript"];
 const supportedTS = ["ts", "typescript"];
 const supportedPY = ["py", "python"];
-const SupportedBrowserLanguages = [...supportedJS, ...supportedTS, ...supportedPY];
+const supportedRuby = ["rb", "ruby"];
+const SupportedBrowserLanguages = [
+  ...supportedJS,
+  ...supportedTS,
+  ...supportedPY,
+  ...supportedRuby,
+];
 const supportedServerLanguages = [...supportedJS, ...supportedTS];
 
 function isJavaScript(language: string) {
@@ -25,6 +31,10 @@ function isTypeScript(language: string) {
 
 function isPython(language: string) {
   return supportedPY.includes(language);
+}
+
+function isRuby(language: string) {
+  return supportedRuby.includes(language);
 }
 
 export function isRunnableInBrowser(language: string) {
@@ -148,6 +158,39 @@ async function runPython(code: string) {
   return executionResult;
 }
 
+async function runRuby(code: string) {
+  const { WASI } = await import("@antonz/runno");
+  const url = "https://unpkg.com/@antonz/ruby-wasi/dist/ruby.wasm";
+
+  // Use captureConsole to capture console output
+  const executionResult = await captureConsole(async () => {
+    const executionPromise = new Promise<void>((resolve, reject) => {
+      WASI.start(fetch(url), {
+        args: ["ruby", "-e", code],
+        stdout: (out) => {
+          console.log(out);
+        },
+        stderr: (err) => {
+          console.error(err);
+        },
+      })
+        .then((result) => {
+          if (result.exitCode === 0) {
+            resolve(); // Resolves the promise with no value
+          } else {
+            reject(new Error("Script execution failed")); // Rejects the promise if execution failed
+          }
+        })
+        .catch((error) => {
+          reject(error); // Reject on error
+        });
+    });
+    return executionPromise;
+  });
+
+  return executionResult;
+}
+
 // Load esbuild lazily, only on demand, for size savings
 async function loadEsBuild() {
   try {
@@ -189,6 +232,9 @@ export async function runCode(code: string, language: string) {
   }
   if (isPython(language)) {
     return runPython(code);
+  }
+  if (isRuby(language)) {
+    return runRuby(code);
   }
   throw new Error(`Unsupported language: ${language}`);
 }
