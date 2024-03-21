92cbc4d571f3b7be50570c547e67bfb4dfa4462f
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-03-21 11:27:55 -0400

    began adding handleCopy and useEffect based on MermaidPreview component

--- src/components/NomnomlPreview.tsx
+++ src/components/NomnomlPreview.tsx
@@ -1,12 +1,34 @@
-import { type ReactNode } from "react";
+import { useRef, type ReactNode, useCallback, useEffect } from "react";
 import nomnoml from "nomnoml";
+import { useClipboard } from "@chakra-ui/react";
+import { useAlert } from "../hooks/use-alert";
+import { nanoid } from "nanoid";
 
 type NomnomlPreviewProps = {
   children: ReactNode & ReactNode[];
 };
 
 const NomnomlPreview = ({ children }: NomnomlPreviewProps) => {
+  const { onCopy, value, setValue } = useClipboard("");
+  const { info } = useAlert();
+  const diagramRef = useRef<HTMLDivElement | null>(null);
   const code = String(children);
+
+  const handleCopy = useCallback(() => {
+    onCopy();
+    info({
+      title: "Copied to Clipboard",
+      message: "Nomnoml SVG diagram was copied to your clipboard.",
+    });
+  }, [onCopy, info]);
+
+  // Render the diagram as an SVG into our card's body
+  useEffect(() => {
+    const diagramDiv = diagramRef.current;
+    if (!diagramDiv) {
+      return;
+    }
+  });
 };
 
 // Memoize to reduce re-renders/flickering when content hasn't changed
