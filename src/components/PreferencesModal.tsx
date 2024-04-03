8763c39536e09874b412df4babcda12f57a8524e
Amnish Singh Arora <78865303+Amnish04@users.noreply.github.com>
2024-04-03 19:02:01 -0400

    Replace "Cancel Adding Provider" SVG with an IconButton (#554)
    * Replace cancel adding provider svg with IconButton
    
    * Fix alignment of cross button with checkboxes
    
    * Prevent other columns from being pushed away when new provider form is shown

--- src/components/PreferencesModal.tsx
+++ src/components/PreferencesModal.tsx
@@ -577,35 +577,37 @@ function PreferencesModal({ isOpen, onClose, finalFocusRef }: PreferencesModalPr
                   <Tbody>
                     {newCustomProvider && (
                       <Tr>
-                        <Td sx={{ textAlign: "center" }}>
-                          <Box
-                            as={MdCancel}
+                        <Td>
+                          <IconButton
+                            aria-label="Cancel adding new provider"
+                            icon={<MdCancel />}
+                            size={"xs"}
                             onClick={() => setNewCustomProvider(null)}
                             onKeyDown={(e) => {
                               if (e.key === "Enter" || e.key === " ") {
                                 setNewCustomProvider(null);
                               }
                             }}
+                            variant="outline"
                             tabIndex={0}
-                            sx={{
-                              cursor: "pointer",
-                              color: "gray",
-                              fontSize: "16px",
-                              display: "inline-flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              borderColor: "gray.200",
-                              borderRadius: "50%",
-
-                              _hover: {
-                                borderColor: "gray.400",
-                                color: "gray.400",
-                              },
+                            color={"grey"}
+                            border={"none"}
+                            p={0}
+                            fontSize={16}
+                            borderRadius={"50%"}
+                            _hover={{
+                              borderColor: "gray.400",
+                              color: "gray.400",
+                            }}
+                            _focus={{
                               _focus: {
                                 outline: "none",
                                 boxShadow: "0 0 0 3px rgba(66, 153, 225, 0.6)",
                                 borderColor: "blue.300",
                               },
+                            }}
+                            _active={{
+                              backgroundColor: "none",
                             }}
                           />
                         </Td>
@@ -699,10 +701,12 @@ function PreferencesModal({ isOpen, onClose, finalFocusRef }: PreferencesModalPr
                         return (
                           <Tr key={provider.name}>
                             <Td>
-                              <Checkbox
-                                onChange={() => handleSelectedProviderChange(provider)}
-                                isChecked={selectedProvider?.name === provider.name}
-                              />
+                              <Flex width={6} justifyContent={"center"}>
+                                <Checkbox
+                                  onChange={() => handleSelectedProviderChange(provider)}
+                                  isChecked={selectedProvider?.name === provider.name}
+                                />
+                              </Flex>
                             </Td>
                             <Td fontSize="xs">{provider.name}</Td>
                             <Td fontSize="xs">
