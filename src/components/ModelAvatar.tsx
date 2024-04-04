b0d6d6d993410dc63c5fc896892d52d894aef371
Katie Liu <kliu57@myseneca.ca>
2024-04-04 08:40:22 -0400

    Logo bg color fix for all models that don't have logo

--- src/components/ModelAvatar.tsx
+++ src/components/ModelAvatar.tsx
@@ -12,14 +12,12 @@ export default function ModelAvatar({ model, size }: { model: ChatCraftModel; si
   if (id.includes("gpt-3.5-turbo")) {
     return <Avatar size={size} bg="#75AB9C" src={logoUrl} title={prettyModel} />;
   }
-  if (id.includes("free") || id === "auto") {
-    return <Avatar size={size} bg="#75AB9C" src={logoUrl} title={prettyModel} />;
-  }
 
   // For now, all the rest use the same colour, or just the logo's background
   return (
     <Avatar
       size={size}
+      bg="#75AB9C"
       showBorder
       borderColor="gray.100"
       _dark={{ borderColor: "gray.600" }}
