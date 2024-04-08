a211e8eb95ecd561de42e4325e50b4eccdc437dd
YumeiWang <yumei.wang@mohawkcollege.ca>
2024-04-08 17:21:39 -0400

    check getTitle first

--- functions/api/share/[[user_id]].ts
+++ functions/api/share/[[user_id]].ts
@@ -40,13 +40,13 @@ async function generateUserFeed(env: Env, user: string): Promise<void> {
       const document = new DOMParser().parseFromString(chatContent, "text/html");
 
       const getTitle = document.querySelector('meta[property="og:title"]');
-      const title = getTitle.getAttribute("content") ?? "No Title";
+      const title = getTitle ? getTitle.getAttribute("content") : "No Title";
 
       const getSummary = document.querySelector('meta[property="og:description"]');
-      const summary = getSummary.getAttribute("content") ?? "No Summary";
+      const summary = getSummary ? getSummary.getAttribute("content") : "No Summary";
 
       const getUrl = document.querySelector('meta[property="og:url"]');
-      const url = getUrl.getAttribute("content") ?? "No URL";
+      const url = getUrl ? getUrl.getAttribute("content") : "No URL";
 
       const id = url.split("/").pop() || "No ID";
 
