0ca21d498d4088959c59efbbe28d8fc757a55668
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-05-31 13:14:58 -0400

    experimenting with removing server-side share target handler

--- service-worker/share-target.ts
+++ service-worker/share-target.ts
@@ -1,13 +1,13 @@
-const handleShareTargetRequest = async () => {
-  try {
-    console.log("trying to fetch");
-    const response = await fetch("/_web-share-target");
-    const data = await response.text();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const handleShareTargetRequest = async () => {
+//   try {
+//     console.log("trying to fetch");
+//     const response = await fetch("/_web-share-target");
+//     const data = await response.text();
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-// Call function when share target request triggered
-handleShareTargetRequest();
+// // Call function when share target request triggered
+// handleShareTargetRequest();
