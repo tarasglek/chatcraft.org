6347939931aa596c8c1c57f037fbfc8ea0a5a990
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-06-06 11:31:02 -0400

    moved skipWaiting() and clientsClaim() to before fetch listener, updated denylist, added DEV block that prevents caching in DEV mode, removed message listener, re-enabled offline support

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -10,13 +10,10 @@ import { NavigationRoute, registerRoute } from "workbox-routing";
 import { CacheableResponsePlugin } from "workbox-cacheable-response";
 import { NetworkFirst } from "workbox-strategies";
 import { ExpirationPlugin } from "workbox-expiration";
+import { clientsClaim } from "workbox-core";
 import { onShareTarget } from "./share-target";
 
 declare const self: ServiceWorkerGlobalScope;
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
-});
 
 const entries = self.__WB_MANIFEST;
 // if (import.meta.env.DEV) {
@@ -30,45 +27,33 @@ cleanupOutdatedCaches();
 
 // allow only fallback in dev: don't cache anything
 let allowlist: undefined | RegExp[];
-// if (import.meta.env.DEV) {
-//   allowlist = [/^\/$/];
-// }
+if (import.meta.env.DEV) {
+  allowlist = [/^\/$/];
+}
 // deny api and server page calls
 const denylist: undefined | RegExp[] = [
-  /^\/api\//,
-  /^\/login\//,
-  /^\/oauth\//,
-  /^\/signin\//,
+  // exclude web-share-target
   /^\/web-share-target\//,
-  // exclude emoji: has its own cache
-  /^\/emojis\//,
-  // exclude sw: if the user navigates to it, fallback to index.html
-  /^\/sw.js$/,
-  // exclude webmanifest: has its own cache
-  /^\/manifest-(.*).webmanifest$/,
 ];
 
-// only cache pages and external assets on local build + start or in production
-// if (import.meta.env.PROD) {
-// include webmanifest cache
-registerRoute(
-  ({ request, sameOrigin }) => sameOrigin && request.destination === "manifest",
-  new NetworkFirst({
-    cacheName: "chatcraft-webmanifest",
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [200] }),
-      // we only need a few entries
-      new ExpirationPlugin({ maxEntries: 100 }),
-    ],
-  })
-);
-// }
+// // only cache pages and external assets on local build + start or in production
+// // if (import.meta.env.PROD) {
+// // include webmanifest cache
+// registerRoute(
+//   ({ request, sameOrigin }) => sameOrigin && request.destination === "manifest",
+//   new NetworkFirst({
+//     cacheName: "chatcraft-webmanifest",
+//     plugins: [
+//       new CacheableResponsePlugin({ statuses: [200] }),
+//       // we only need a few entries
+//       new ExpirationPlugin({ maxEntries: 100 }),
+//     ],
+//   })
+// );
+// // }
 
 // allow work offline
-// registerRoute(new NavigationRoute(createHandlerBoundToURL("/"), { allowlist, denylist }));
-
-// share target handler
-self.addEventListener("fetch", onShareTarget);
+registerRoute(new NavigationRoute(createHandlerBoundToURL("/"), { allowlist, denylist }));
 
 // self.addEventListener("fetch", async (event) => {
 //   console.log("Request has been intercepted");
@@ -83,3 +68,9 @@ self.addEventListener("fetch", onShareTarget);
 //     });
 //   }
 // });
+
+self.skipWaiting();
+clientsClaim();
+
+// share target handler
+self.addEventListener("fetch", onShareTarget);
