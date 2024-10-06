232b7a0e6d2333fefeff89a60fe4830c081696c1
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-10-06 18:06:27 -0400

    upgrade eslint to v9
    migrate config from .eslintrc.json and .eslintignore to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js"],
+  },
+  {
+    ignores: [
+      "**/.github/",
+      "**/.hustky/",
+      "**/.vscode/",
+      "**/build/",
+      "**/docs/",
+      "**/node_modules/",
+      "**/public/",
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:react-hooks/recommended",
+      "eslint-config-prettier"
+    )
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      react: fixupPluginRules(react),
+      "react-hooks": fixupPluginRules(reactHooks),
+      "jsx-a11y": fixupPluginRules(jsxA11Y),
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
+
+      "react/react-in-jsx-scope": "off",
+      "react/no-children-prop": "off",
+      "jsx-a11y/no-autofocus": "off",
+      "jsx-a11y/iframe-has-title": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-function": "off",
+    },
+  },
+];
