e18404d12e87ab856c75b5de6ef55082fb6672cc
krins <krinskumar@gmail.com>
2024-11-19 17:37:25 -0500

    removed RSS feed functions and use

--- functions/api/share/[user].test.ts
+++
@@ -1,6 +0,0 @@
-import { describe, test } from "vitest";
-
-describe("/api/share/:user", () => {
-  // Need ability to work with r2 - https://github.com/cloudflare/miniflare/issues/600
-  test.skip("/api/share/:user", () => {});
-});
