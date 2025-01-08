edbfa99dc2b73d439db76043c162b25c3234ae74
Taras Glek <taras@glek.net>
2025-01-08 16:14:49 +0200

    refactor: Replace fsrouter with new route discovery mechanism using @http/discovery

--- deno/main.ts
+++ deno/main.ts
@@ -1,12 +1,36 @@
 import { serveDir } from "jsr:@std/http/file-server";
-import * as fsrouter from "jsr:@pomdtr/fsrouter";
 import { route, type Route } from "jsr:@std/http/unstable-route";
+import freshPathMapper from "jsr:@http/discovery/fresh-path-mapper";
+import { discoverRoutes } from "jsr:@http/discovery/discover-routes";
+import { asSerializablePattern } from "jsr:@http/discovery/as-serializable-pattern";
+import { byPattern } from "jsr:@http/route/by-pattern";
+
+async function cfRoutes(fileRootUrl: string) {
+  const routes = await discoverRoutes({
+    pattern: "/",
+    fileRootUrl: fileRootUrl,
+    pathMapper: freshPathMapper,
+    verbose: true,
+  });
+
+
+  const handlers = [];
+
+  for (const { pattern, module } of routes) {
+    console.log("\nPattern:", asSerializablePattern(pattern));
+    console.log("Module:", module.toString());
+    const routeModule = await import(module.toString());
+    console.log("Default:", routeModule.default);
+    handlers.push(byPattern(pattern, routeModule.default));
+  }
+  return handlers;
+}
 
 // watchexec --verbose -i deno/** pnpm build
 // https://jsr.io/@http/route and https://github.com/jollytoad/deno_http_fns/tree/main/packages/examples has newer fancier stuff
 async function handleFetch(req: Request): Promise<Response> {
-  const routes = fsrouter.discoverRoutes("./functions") as Route[];
-  console.log(`${JSON.stringify(routes.map((x) => x.pattern.pathname))}`);
+  const routes = await cfRoutes(import.meta.resolve("../functions")) as Route[];
+  console.log(`${JSON.stringify(routes.map((x) => { p: x.pattern.pathname }))}`);
 
   return route(routes, async (req: Request) => {
     console.log("fallback", req);
