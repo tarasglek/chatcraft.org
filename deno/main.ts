68fa3eeaae28ec54dda4d61e50d1806080b3cb9c
Taras Glek <taras@glek.net>
2025-01-08 14:56:35 +0200

    deno stuff

--- deno/main.ts
+++ deno/main.ts
@@ -1,8 +1,21 @@
 import { serveDir } from "jsr:@std/http/file-server";
+import * as fsrouter from "jsr:@pomdtr/fsrouter";
+import { route, type Route } from "jsr:@std/http/unstable-route";
 
-/** run `pnpm build --watch`  */
-function handleFetch(req: Request): Promise<Response> {
-  return serveDir(req, { fsRoot: "build" });
+// watchexec --verbose -i deno/** pnpm build
+// https://jsr.io/@http/route and https://github.com/jollytoad/deno_http_fns/tree/main/packages/examples has newer fancier stuff
+async function handleFetch(req: Request): Promise<Response> {
+  const routes = fsrouter.discoverRoutes("./functions") as Route[];
+  console.log(`${JSON.stringify(routes.map((x) => x.pattern.pathname))}`);
+
+  return route(routes, async (req: Request) => {
+    console.log("fallback", req);
+    const ret = await serveDir(req, { fsRoot: "build" });
+    console.log(ret);
+    return ret;
+  })(req);
+
+  // return ;
 }
 
 export default {
---
+++ deno/serve-ssl.ts
@@ -0,0 +1,25 @@
+import handler from "./main.ts";
+// deno run --unstable-sloppy-imports --unstable-net --watch -A serve-ssl.ts
+let keys: string[];
+do {
+  try {
+    keys = await Promise.all(["cert", "key"].map((name) => Deno.readTextFile(`${name}.pem`)));
+    break;
+  } catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+      console.log("SSL certificates not found, generating new ones...");
+      await new Deno.Command("sh", {
+        args: [
+          "-c",
+          "openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj '/CN=localhost'",
+        ],
+      }).output();
+      continue;
+    }
+    throw error;
+  }
+} while (true);
+
+const [cert, key] = keys;
+Deno.chdir("..");
+Deno.serve({ cert, key, port: 4443, reusePort: true }, handler.fetch);
