69c31b4ea5b23c836ebf380605ce6f513fb6ace4
Taras Glek <taras@glek.net>
2025-01-09 14:30:13 +0200

    feat: Add lastlogin integration for dev environment in login API

--- functions/api/login.ts
+++ functions/api/login.ts
@@ -1,6 +1,8 @@
-import { createResourcesForEnv } from "../utils";
-import { handleGithubLogin } from "../github";
-import { handleGoogleLogin } from "../google";
+import { createResourcesForEnv, errorResponse } from "../utils";
+import { handleGithubLogin, requestDevUserInfo } from "../github";
+import { handleGoogleLogin, requestGoogleDevUserInfo } from "../google";
+import { lastlogin } from "@pomdtr/lastlogin";
+import type { LastLoginOptions } from "@pomdtr/lastlogin";
 
 interface Env {
   ENVIRONMENT: string;
@@ -32,6 +34,12 @@ export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
     }
   }
 
+  if (!provider) {
+    provider = "github";
+  } else if (provider !== "google" && provider !== "github") {
+    return errorResponse(403, `Invalid provider: ${provider}`);
+  }
+
   // Include ?chat_id=... to redirect back to a given chat in the client.  Google will
   // return it back to us via ?state=provider%3Dgoogle%26chat_id%3Dl77...
   let chatId = reqUrl.searchParams.get("chat_id");
@@ -47,7 +55,39 @@ export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
   const code = reqUrl.searchParams.get("code");
 
   const { appUrl, isDev, tokenProvider } = createResourcesForEnv(env.ENVIRONMENT, request.url);
+  // use lastlogin for dev
+  if (isDev) {
+    const wrapped_fetch = lastlogin(async (request) => {
+      const email = request.headers.get("X-Lastlogin-Email");
+      console.log(`X-Lastlogin-Email ${email}!`);
+      if (!email) {
+        return errorResponse(403, "Lastlogin failed us");
+      }
+      const avatarUrl = (provider === "google" ? requestGoogleDevUserInfo() : requestDevUserInfo()).avatarUrl;
+      // User info goes in a non HTTP-Only cookie that browser can read
+      const idToken = await tokenProvider.createToken(email, { username: email, name: email, avatarUrl }, JWT_SECRET);
+      // API authorization goes in an HTTP-Only cookie that only functions can read
+      const accessToken = await tokenProvider.createToken(email, { role: "api" }, JWT_SECRET);
 
+      // Return to the root or a specific chat if we have an id
+      const url = new URL(chatId ? `/c/${chatId}` : "/", appUrl).href;
+
+      return new Response(null, {
+        status: 302,
+        headers: new Headers([
+          ["Location", url],
+          ["Set-Cookie", tokenProvider.serializeToken("access_token", accessToken)],
+          ["Set-Cookie", tokenProvider.serializeToken("id_token", idToken)],
+        ]),
+      });
+
+    }, {
+      provider: provider as LastLoginOptions["provider"],
+      verifyEmail: _ => true, // we accept all emails
+      secretKey: JWT_SECRET
+    })
+    return wrapped_fetch(request);
+  }
   if (provider === "google") {
     return handleGoogleLogin({
       isDev,
