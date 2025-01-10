f43e5f624eccc98d013dfe77fe7376b46b74a118
Taras Glek <taras@glek.net>
2025-01-10 15:53:32 +0200

    feat: Add JSON import and table creation to DuckCommand

--- src/lib/commands/DuckCommand.ts
+++ src/lib/commands/DuckCommand.ts
@@ -7,6 +7,7 @@ import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url";
 import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url";
 import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url";
 import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url";
+import type { Utf8 } from "apache-arrow";
 
 const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
   mvp: {
@@ -23,8 +24,8 @@ async function duckdb_start() {
   const bundle = await duckdb_lib.selectBundle(MANUAL_BUNDLES);
   // Instantiate the asynchronus version of DuckDB-wasm
   const worker = new Worker(bundle.mainWorker!);
-  const logger = new duckdb.ConsoleLogger();
-  const duckdb = new duckdb.AsyncDuckDB(logger, worker);
+  const logger = new duckdb_lib.ConsoleLogger();
+  const duckdb = new duckdb_lib.AsyncDuckDB(logger, worker);
   await duckdb.instantiate(bundle.mainModule, bundle.pthreadWorker);
   return duckdb;
 }
@@ -35,6 +36,7 @@ async function idbExport(): Promise<Blob> {
   const blob = await exportDB(db);
   return blob;
 }
+
 export class DuckCommand extends ChatCraftCommand {
   constructor() {
     super("duck", "/duck", "Do some SQL queries");
@@ -45,14 +47,37 @@ export class DuckCommand extends ChatCraftCommand {
     // const duckdb = await import("@duckdb/duckdb-wasm");
     // Select a bundle based on browser checks
     const duckdb = await duckdb_start();
+    const jsonBlob = await idbExport();
+    await duckdb.registerFileBuffer("dexie.json", new Uint8Array(await jsonBlob.arrayBuffer()));
+    const sql_import_dexie_json = `CREATE TABLE dexie_json AS
+WITH json_data AS (
+    SELECT
+        unnest(data.data) AS data
+    FROM
+        read_json('dexie.json', maximum_object_size=${jsonBlob.size})
+)
+SELECT
+    data.tableName AS table_name,
+    data.rows AS rows
+FROM
+    json_data;`;
+    const sql_select_dexie_tables = `SELECT table_name FROM dexie_json`;
+    const c = await duckdb.connect();
+    globalThis.window.c = c;
+    await c.query(sql_import_dexie_json);
+    const json = (await c.query<{ table_name: Utf8 }>(sql_select_dexie_tables)).toArray();
+
     const message = [
       "## SQL\n",
-      "| Operation | Count | min/avg/max (ms) | Total (ms) |",
-      "|-----------|--------|-----------------|------------|",
-      `${duckdb}`,
+      "```json",
+      `${JSON.stringify(json, null, 2)}`,
+      "```",
       // ...results.map((r) => `| ${r.name} | ${r.ops} | ${r.min}/${r.avg}/${r.max} | ${r.total} |`),
     ].join("\n");
-
+    (globalThis.window as any).db = {
+      duckdb,
+      jsonBlob,
+    };
     return chat.addMessage(new ChatCraftAppMessage({ text: message }));
   }
 }
