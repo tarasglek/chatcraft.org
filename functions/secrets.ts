c17f0195d13a414f84c60a830bf95cdd3e91d5ec
Taras Glek <taras@glek.net>
2025-01-20 15:00:35 +0200

    secrets

---
+++ functions/secrets.ts
@@ -0,0 +1,12 @@
+import { decryptSops } from "sops-age";
+import production_json from "production.json" with { type: "json" };
+
+export async function get_secrets(environment: string, AGE_SECRET_KEY: string) {
+  switch (environment) {
+    case "production":
+      return await decryptSops(production_json, {
+        secretKey: AGE_SECRET_KEY,
+      });
+  }
+  throw new Error("Unknown environmentp");
+}
