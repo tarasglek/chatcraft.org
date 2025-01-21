93684a8a02190d1fa742f9977da25cce95e632c7
Taras Glek <taras@glek.net>
2025-01-21 13:28:07 +0200

    lint

--- functions/api/user.ts
+++ functions/api/user.ts
@@ -1,4 +1,4 @@
-import { get_secrets } from "../secrets";
+// import { get_secrets } from "../secrets";
 
 interface Env {
   JWT_SECRET: string;
@@ -6,7 +6,7 @@ interface Env {
 
 // GET https://chatcraft.org/api/proxy?url=<encoded url...>
 // Must include JWT in cookie, and user must match token owner
-export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
+export const onRequestGet: PagesFunction<Env> = async () => {
   console.error("user-info.ts onRequestGet");
   // get_secrets("production", env.JWT_SECRET)
   return new Response(JSON.stringify({ hello: "world" }), {
