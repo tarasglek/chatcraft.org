d265ba9ff1732eb42920057331d264d876ae5519
Taras Glek <taras@glek.net>
2025-02-25 12:32:15 +0200

    Add react scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// src/index.jsx
+import { scan } from "react-scan"; // must be imported before React and React DOM
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
@@ -5,13 +7,21 @@ import { RouterProvider } from "react-router-dom";
 
 import router from "./router";
 import theme from "./theme";
-import { SettingsProvider } from "./hooks/use-settings";
+import { SettingsProvider, useSettings } from "./hooks/use-settings";
 import { UserProvider } from "./hooks/use-user";
 import { ModelsProvider } from "./hooks/use-models";
 import { CostProvider } from "./hooks/use-cost";
 import { AudioPlayerProvider } from "./hooks/use-audio-player";
 import { WebHandlersProvider } from "./hooks/use-web-handlers";
 import { ChatProvider } from "./hooks/use-chat";
+
+// need to import and call it
+scan({
+  enabled: false,
+});
+
+// enable perf profiling by typing in dev console: window.scan({enabled:true})
+(globalThis.window as any).scan = scan;
 
 ReactDOM.createRoot(document.querySelector("main") as HTMLElement).render(
   <React.StrictMode>
